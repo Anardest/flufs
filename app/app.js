801825dc0e18aa32456c2db79befd7bcfd21904d
Anardest <associalgamer@gmail.com>
2025-08-09 19:16:51 +0300

    +поддержка json, статики, роутов

--- app/app.js
+++ app/app.js
@@ -17,6 +17,15 @@ const PORT = process.env.PORT;
 
 
 app.use(morgan('dev'));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const staticPagesRouter = require('./routes/staticPages');
+app.use('', staticPagesRouter);
+
+const groupsRouter = require('./routes/groups');
+app.use('/api/groups', groupsRouter);
 
 
 app.listen(PORT, () => {
