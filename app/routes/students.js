5a082a83f7cbc1a8977fd91a5fcb5ede9b6c9157
Anardest <associalgamer@gmail.com>
2025-08-09 19:18:12 +0300

    Роуты студентов

---
+++ app/routes/students.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const router = Router();
+const ApiResponse = require('../utils/apiResponse');
+const Validator = require('../utils/validator');
+const knex = require('../database');
+
+router.get('', async (req, res) => {
+    try {
+        //Будем получать студентов в зависимости от ссылки
+    } catch (error) {
+        
+    }
+})
+
+module.exports = router;
