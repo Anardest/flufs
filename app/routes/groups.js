e5bb8e1673fc3bf41a7b37e475b344d24d548f31
Anardest <associalgamer@gmail.com>
2025-08-11 21:23:31 +0300

    Новые роуты

--- app/routes/groups.js
+++ app/routes/groups.js
@@ -3,8 +3,9 @@ const router = Router();
 const ApiResponse = require('../utils/apiResponse');
 const Validator = require('../utils/validator');
 const knex = require('../database');
+const groupsFunctions = require('../utils/groupsFunc');
 
-router.get('/', async (req, res) => {
+router.get('/get', async (req, res) => {
     try {
         const groups = await knex('groups').select('*');
         return ApiResponse.success(res, groups);
@@ -29,6 +30,74 @@ router.post('/create', async (req, res) => {
         ApiResponse.error(res, error.message);
     }
 });
+
+//Будем получать студентов в зависимости от ссылки
+router.get('/:id/students', async (req, res) => {
+    try {
+        const groupId = req.params.id;
+        const cheker = await groupsFunctions.checkGroup(groupId);
+
+        const students = await knex('students')
+            .where({ group_id: groupId })
+            .select('student_id', 'first_name', 'last_name');
+
+        if (students.length === 0) {
+            return ApiResponse.success(res, [], 'Группа найдена, но студентов нет.')
+        }
+        return ApiResponse.success(res, students);
+
+    } catch (error) {
+        console.error('Ошибка получения студентов:', error);
+        return ApiResponse.error(res, 'Ошибка получения студентов:', error);
+    }
+});
+
+router.post('/:id/add_student', async (req, res) => {
+    try {
+        const groupId = req.params.id;
+        const cheker = await groupsFunctions.checkGroup(groupId);
+
+        const { first_name, last_name } = req.body;
+        if (!Validator.isString(first_name) || !Validator.isString(last_name)) {
+            return ApiResponse.error(res, 'Имя и фамилия должны быть строками', `Ответ: ${first_name} - ${last_name}`);
+        }
+
+        const [newStudent] = await knex('students')
+            .insert({
+                first_name: first_name,
+                last_name: last_name,
+                group_id: groupId
+            }).returning('*');
+
+        return ApiResponse.success(res, newStudent);
+    } catch (error) {
+        return ApiResponse.error(res, 'Ошибка создания студента', error);
+    }
+});
+
+router.delete('/:id/force_delete', async (req, res) => {
+    try {
+        const groupId = req.params.id;
+        const cheker = await groupsFunctions.checkGroup(groupId);
+
+        const existingGroup = await groupsFunctions.checkGroup(groupId);
+
+        await knex.transaction(async (trx) => {
+
+            await trx('students')
+                .where({ group_id: groupId })
+                .del();
+            
+            await trx('groups')
+                .where({ group_id: groupId })
+                .del();
+        });
+        return ApiResponse.success(res, null, 'Группа и все студенты успешно удаленыю')
+    } catch (error) {
+        return ApiResponse.error(res, 'Не удалось удалить группу', error.message, 500);
+    }
+})
+
 
 
 module.exports = router;
--- app/routes/students.js
+++ app/routes/students.js
@@ -3,13 +3,67 @@ const router = Router();
 const ApiResponse = require('../utils/apiResponse');
 const Validator = require('../utils/validator');
 const knex = require('../database');
+const studentFunctions = require('../utils/studentsFunc');
+const groupsFunctions = require('../utils/groupsFunc');
 
-router.get('', async (req, res) => {
+router.put('/:id/edit', async (req, res) => {
     try {
-        //Будем получать студентов в зависимости от ссылки
-    } catch (error) {
+        const studentId = req.params.id;
+        const existingStudent = await studentFunctions.checkStudent(studentId);
+
+        const { first_name, last_name, group_id } = req.body;
         
+        const updates = {};
+        if (first_name) {
+            updates.first_name = first_name;
+        }
+        if (last_name) {
+            updates.last_name = last_name;
+        }
+        if (group_id) {
+            const existingGroup = await groupsFunctions.checkGroup(group_id);
+            updates.group_id = group_id;
+        }
+        
+        // Проверяем, есть ли что-то для обновления
+        if (Object.keys(updates).length === 0) {
+            return ApiResponse.error(res, 'Нет данных для обновления', null, 400);
+        }
+
+        const updatedCount = await knex('students')
+            .where({ student_id: studentId })
+            .update(updates);
+        
+        if (updatedCount.length === 0) {
+            return ApiResponse.error(res, 'Студент с указанным ID не найден');
+        }
+
+        // Получаем обновленную запись для возврата в ответе
+        const [updatedStudent] = await knex('students').where({ student_id: studentId }).select('*');
+
+        return ApiResponse.success(res, updatedStudent, `Студент с ID ${studentId} успешно обновлен.`);
+    } catch (error) {
+        return ApiResponse.error(res, 'Не удалось обновить студента', error.message, 500);
     }
-})
+});
+
+router.delete('/:id/delete', async (req, res) => {
+    try {
+        const studentId = req.params.id;
+        await studentFunctions.checkStudent(studentId);
+
+        const deletedCount = await knex('students')
+            .where({ student_id: studentId })
+            .del();
+        
+        if (deletedCount === 0) {
+            return ApiResponse.error(res, 'Студент не найден', null, 400);
+        }
+
+        return ApiResponse.success(res, null, 'Студент удалён');
+    } catch (error) {
+        return ApiResponse.error(res, 'Не удалось удалить студента', error.message, 500);
+    }
+});
 
 module.exports = router;
